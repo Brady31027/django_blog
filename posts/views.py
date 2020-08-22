c61927f857e60552207c28bf0b8308c152ec06d5
Brady Cheng <brady3127@gmail.com>
2020-08-21 21:56:23 -0700

    first template

--- posts/views.py
+++ posts/views.py
@@ -3,17 +3,42 @@ from django.http import HttpResponse
 
 # Create your views here.
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    context = {
+        "title": "Create",
+    }
+    return render(request, "index.html", context)
 
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    context = {
+        "title": "Detail",
+    }
+    return render(request, "index.html", context)
 
 def post_list(request):
-    return HttpResponse("<h1>List</h1>")
+    #if request.user.is_authenticated:
+    #    context = {
+    #        "title": "List",
+    #        "user": request.user,
+    #    }
+    #else:
+    #    context = {
+    #        "title": "You need to login first",
+    #        "user": request.user,
+    #    }
+    context = {
+        "title": "List",
+    }
+    return render(request, "index.html", context)
 
 def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+    context = {
+        "title": "Update",
+    }
+    return render(request, "index.html", context)
 
 def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+    context = {
+        "title": "Delete",
+    }
+    return render(request, "index.html", context)
 
